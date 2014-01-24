dea8886d6392435f829f0762c1633005cbf19cd4
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2014-01-24 23:00:01 +0800

    Basic structure built. Algorith is next

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@ ref: http://lxml.de/objectify.html
 """
 #from objectifiedJson import objectJSONEncoder
 from lxml import objectify
+from lxml import etree
 
 #class definitions
 class Workunit:
@@ -43,8 +44,9 @@ class HostAvailability:
 WR_STATE_EMPTY = -1
 WR_STATE_PRESENT = -2
 
-f = open("xs_input.xml", "r")
-text = f.read()
+inFile = open("xs_input.xml", "r")
+text = inFile.read()
+inFile.close()
 
 xsInput = objectify.fromstring(text)
 
@@ -58,6 +60,8 @@ for platform in xsInput.project.shared_mem.platforms.platform:
     print "Platform name: ", platform.name
 """
 #Results
+
+"""
 wuResults = list()
 nWUResults = len(xsInput.project.shared_mem.wu_results.wu_result)
 
@@ -79,7 +83,9 @@ for wuResult in wuResults:
     print "WU Id:", wuResult.workunit.id
     print "Name: ", wuResult.workunit.name
     print "Fpops: ", wuResult.workunit.estFpops
-    
+"""
+
+"""
 #Requests
 requests = list()
 nRequests = len(xsInput.clients.requests.scheduler_request)
@@ -89,8 +95,57 @@ for schedulerRequest in xsInput.clients.requests.scheduler_request:
     request = Request()
     
     request.hostid = schedulerRequest.hostid
-    request.workRequest = schedulerRequest.work_req_seconds
-         
+    request.workRequested = schedulerRequest.work_req_seconds
+    request.availabilityRecords = list()
+    
+    for record in xsInput.clients.requests.scheduler_request.host_availability.record:
+        
+        availabilityRecord = HostAvailability()        
+        availabilityRecord.hour = record.time.hour
+        availabilityRecord.minute = record.time.minute
+        availabilityRecord.year = record.date.year
+        availabilityRecord.month = record.date.month
+        availabilityRecord.day = record.date.day
+        availabilityRecord.availability = record.availability
+        
+        #print "Year ", availabilityRecord.year, " Month ", availabilityRecord.month, " Day ", availabilityRecord.day, " Hour ", availabilityRecord.hour, " Minute ", availabilityRecord.minute, " Availability ", availabilityRecord.availability
+        
+        request.availabilityRecords.append(availabilityRecord)
+        
+    
+"""
+
+#Write assignment
+
+"""
+        <host_id>3</host_id>
+        <wu_id>2</wu_id>
+        <result_id>5</result_id>
+"""
+
+"""
+root = etree.Element("xs_output")
+#assignment = etree.SubElement(root, "assignment")
+#hostId = etree.SubElement(assignment, "host_id")
+#wuId = etree.SubElement(assignment, "wu_id")
+#resultId = etree.SubElement(assignment, "result_id")
+
+for i in range(0, 3):
+        
+    assignment = etree.SubElement(root, "assignment")
+    hostId = etree.SubElement(assignment, "host_id")
+    wuId = etree.SubElement(assignment, "wu_id")
+    resultId = etree.SubElement(assignment, "result_id")
+    hostId.text = str(1)
+    wuId.text = str(5)
+    resultId.text = str(38)
+    
+
+outFile = open("xs_output.xml", "w")
+outFile.write(etree.tostring(root, pretty_print=True))
+outFile.close()
+print(etree.tostring(root, pretty_print=True))
+"""
    
 #TODO: grab list of app_versions, assignment, wu_results, nodes
 #sort lists
