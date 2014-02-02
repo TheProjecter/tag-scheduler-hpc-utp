088a3f1f55f30fb70c137665d0c1fd8fc6ce41f2
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2014-02-02 15:43:58 +0800

    Fixed: Single host thermal profile read issue. Structured program.

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 """
 ref: http://lxml.de/objectify.html
+
+Use round robin, FIFO scheme. 1 unit assigned per round
 """
 #from objectifiedJson import objectJSONEncoder
 from lxml import objectify
@@ -14,19 +16,55 @@ class Workunit:
 class WorkunitResult:
     def __init__(self):
         self.workunit = Workunit()
- 
-class Node:
-    def __init__(self):
-        pass
-    
+        
+    def __repr__(self):
+        return '{}: {} {} {}'.format(self.__class__.__name__, self.id, self.workunit.name, self.workunit.id)            
+   
 class Request:
     def __init__(self):
-        pass
+        self.reliability = float(0)
+        self.workAssigned = float(0)
+        
+    def __repr__(self):
+        return '{}: {} {}'.format(self.__class__.__name__, self.hostid, self.reliability)
     
+    """    
+    def __cmp__(self, other):
+        if hasattr(other, 'reliability'):
+            return self.reliability.__cmp__(other.reliability) 
+    """
+    #comparison methods
+    def __eq__(self, other):
+        return self.reliability == other.reliability
+    def __ne__(self, other):
+        return self.reliability != other.reliability
+    def __lt__(self, other):
+        return self.reliability < other.reliability
+    def __gt__(self, other):
+        return self.reliability > other.reliability
+    def __le__(self, other):
+        return self.reliability <= other.reliability
+    def __ge__(self, other):
+        return self.reliability >= other.reliability
+            
 class HostAvailability:
-    def __init__(self):
-        pass
+    def __init__(self, year, month, day, hour, minute, availability):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.availability = availability
     
+class Assignment:
+    def __init__(self, hostId, wuId, resultId):
+        self.hostId = hostId
+        self.wuId = wuId
+        self.resultId = resultId
+        
+    def __repr__(self):
+        return '{}: H{} WU{} R{}'.format(self.__class__.__name__, self.hostid, self.wuId, self.resultId)        
+            
 """
 <time>
     <hour>21</hour>
@@ -44,11 +82,12 @@ class HostAvailability:
 WR_STATE_EMPTY = -1
 WR_STATE_PRESENT = -2
 
-inFile = open("xs_input.xml", "r")
-text = inFile.read()
-inFile.close()
+def readSchedulerInput(filename):
+    inFile = open(filename, "r")
+    text = inFile.read()
+    inFile.close()  
+    return objectify.fromstring(text)
 
-xsInput = objectify.fromstring(text)
 
 """
 print "Shared memory state: ", xsInput.project.shared_mem.ready
@@ -59,25 +98,29 @@ print "Len: ", len(xsInput.project.shared_mem.platforms.platform)
 for platform in xsInput.project.shared_mem.platforms.platform:
     print "Platform name: ", platform.name
 """
-#Results
 
-wuResults = list()
-nWUResults = len(xsInput.project.shared_mem.wu_results.wu_result)
-totalWorkAvailable = 0
-print "Work units:"
-for wuResult in xsInput.project.shared_mem.wu_results.wu_result:
+def readWorkunitResults(xsInput):
+    #Results
+    wuResults = list()
+    #nWUResults = len(xsInput.project.shared_mem.wu_results.wu_result)
+    totalWorkAvailable = 0
+    print "Work units:"
+    for wuResult in xsInput.project.shared_mem.wu_results.wu_result:
+        
+        wuR = WorkunitResult()
+        
+        wuR.state = wuResult.state
+        wuR.id = wuResult.resultid
+        wuR.workunit.id = wuResult.workunit.id
+        wuR.workunit.name = wuResult.workunit.name
+        wuR.workunit.estFpops = wuResult.workunit.rsc_fpops_est
+        totalWorkAvailable += wuResult.workunit.rsc_fpops_est
+        wuResults.append(wuR)
+        
+    print "Is there work available: ", "Yes" if (totalWorkAvailable > 0 ) else "No"
+    print "Workunit results available: ", len(wuResults)
     
-    wuR = WorkunitResult()
-    
-    wuR.state = wuResult.state
-    wuR.id = wuResult.resultid
-    wuR.workunit.id = wuResult.workunit.id
-    wuR.workunit.name = wuResult.workunit.name
-    wuR.workunit.estFpops = wuResult.workunit.rsc_fpops_est
-    totalWorkAvailable += wuResult.workunit.rsc_fpops_est
-    wuResults.append(wuR)
-    
-print "Is there work available: ", "Yes" if (totalWorkAvailable > 0 ) else "No"
+    return wuResults
     
 #for wuResult in wuResults:    
 #    print "R Id: ", wuResult.id
@@ -86,37 +129,44 @@ print "Is there work available: ", "Yes" if (totalWorkAvailable > 0 ) else "No"
 #    print "Name: ", wuResult.workunit.name
 #    print "Fpops: ", wuResult.workunit.estFpops
 
-
-"""
-#Requests
-requests = list()
-nRequests = len(xsInput.clients.requests.scheduler_request)
-
-for schedulerRequest in xsInput.clients.requests.scheduler_request:
+def readRequests(xsInput):
+    #Requests
+    requests = list()
+    #nRequests = len(xsInput.clients.requests.scheduler_request)
     
-    request = Request()
-    
-    request.hostid = schedulerRequest.hostid
-    request.workRequested = schedulerRequest.work_req_seconds
-    request.availabilityRecords = list()
-    
-    for record in xsInput.clients.requests.scheduler_request.host_availability.record:
+    for schedulerRequest in xsInput.clients.requests.scheduler_request:
         
-        availabilityRecord = HostAvailability()        
-        availabilityRecord.hour = record.time.hour
-        availabilityRecord.minute = record.time.minute
-        availabilityRecord.year = record.date.year
-        availabilityRecord.month = record.date.month
-        availabilityRecord.day = record.date.day
-        availabilityRecord.availability = record.availability
+        request = Request()
         
-        #print "Year ", availabilityRecord.year, " Month ", availabilityRecord.month, " Day ", availabilityRecord.day, " Hour ", availabilityRecord.hour, " Minute ", availabilityRecord.minute, " Availability ", availabilityRecord.availability
+        request.hostid = schedulerRequest.hostid
+        request.workRequested = schedulerRequest.work_req_seconds
+        request.availabilityRecords = list()
         
-        request.availabilityRecords.append(availabilityRecord)
+        for record in schedulerRequest.host_availability.record:
+            
+            availabilityRecord = HostAvailability(record.date.year, record.date.month, record.date.day,
+                                                  record.time.hour, record.time.minute,
+                                                  record.availability)
+            """        
+            availabilityRecord.hour = record.time.hour
+            availabilityRecord.minute = record.time.minute
+            availabilityRecord.year = record.date.year
+            availabilityRecord.month = record.date.month
+            availabilityRecord.day = record.date.day
+            availabilityRecord.availability = record.availability
+            """
+            
+            #print "Year ", availabilityRecord.year, " Month ", availabilityRecord.month, " Day ", availabilityRecord.day, " Hour ", availabilityRecord.hour, " Minute ", availabilityRecord.minute, " Availability ", availabilityRecord.availability
+            
+            request.availabilityRecords.append(availabilityRecord)
+            
+        request.reliability = computeHostReliability(request.availabilityRecords)
+            
+        requests.append(request)
         
-    
-"""
-
+    print "Total requests: ", len(requests)
+    return requests
+  
 #Write assignment
 
 """
@@ -125,31 +175,135 @@ for schedulerRequest in xsInput.clients.requests.scheduler_request:
         <result_id>5</result_id>
 """
 
-"""
-root = etree.Element("xs_output")
-#assignment = etree.SubElement(root, "assignment")
-#hostId = etree.SubElement(assignment, "host_id")
-#wuId = etree.SubElement(assignment, "wu_id")
-#resultId = etree.SubElement(assignment, "result_id")
 
-for i in range(0, 3):
+#Perform assignments
+""" 
+TODO: grab list of app_versions, assignment, wu_results, nodes
+sort lists
+    nodes: highest demand to lowest for work + availability
+    wu_results: highest fpops to lowest
+"""
+
+#compute host reliability
+def computeHostReliability(availabilityRecords):
+    
+    #collect last 30 records
+    availableCount = 0
+    
+    for availabilityRecord in availabilityRecords:
+        if availabilityRecord.availability == 1:
+            availableCount += 1
+            
+    reliability = float(availableCount)/float(len(availabilityRecords))
+    
+    print "Count, Reliability, Total: ", availableCount, reliability, len(availabilityRecords)
+    
+    return reliability
+    
+    #return reliability score
+
+#assign work (round robin, raking/sorting: wants work, is available, FIFO
+def scheduleWork(wuResults, requests):
+  
+    #check if there is work to be processed
+    totalWorkAvailable = 0
+    for wuResult in wuResults:
+        totalWorkAvailable += wuResult.workunit.estFpops
+    
+    if totalWorkAvailable == 0:
+        return list()
+    
+    #form list of hosts requesting work
+    hostsRequestingWork = list()
+    
+    for request in requests:
+        if request.workRequested > 0:
+            hostsRequestingWork.append(request)
+    
+    if len(hostsRequestingWork) == 0:
+        return list()
+    
+    print(hostsRequestingWork)
+    
+    #sort hosts by availability, in decreasing order
+    sortedHosts = sorted(hostsRequestingWork, reverse=True)
+    
+    print(sortedHosts)
+    
+    #start round robin assignment
+    return roundRobin(wuResults, sortedHosts) 
+
+def roundRobin(wuResults, hosts):
+    
+    #requestIndex = 0
+    #requestLimitIndex = len(hosts) - 1
+    print wuResults
+    #print hosts
+    
+    assignments = list()   
+    
+    #TODO: check work unit state
+    for wuResult in wuResults:
+        hostIndex = 0
+        assigned = False
+         
+        for host in hosts:
+            
+            #print "Estimate ", host.workAssigned + wuResult.workunit.estFpops, host.workRequested            
+                        
+            if (host.workAssigned + wuResult.workunit.estFpops) <= host.workRequested:    
+                print "Bingo"            
+                assignment = Assignment(host.hostid, wuResult.workunit.id, wuResult.id)
+                assignments.append(assignment)
+                print "Assigning ", assignment
+                host.workAssigned += wuResult.workunit.estFpops
+                assigned = True
+                break
+            #increment host index
+            hostIndex += 1
         
-    assignment = etree.SubElement(root, "assignment")
-    hostId = etree.SubElement(assignment, "host_id")
-    wuId = etree.SubElement(assignment, "wu_id")
-    resultId = etree.SubElement(assignment, "result_id")
-    hostId.text = str(1)
-    wuId.text = str(5)
-    resultId.text = str(38)
+        if assigned == True:
+            #move host that was assigned work to the bottom of the list
+            hosts.insert(len(hosts) - 1, hosts.pop(hostIndex))        
+    
+    return assignments
+    
+def writeSchedulerOutput(filename, assignments): 
+    
+    print "Assignments: ", assignments   
+
+    root = etree.Element("xs_output")
+  
+    for assignment in assignments:
+            
+        assignmentElement = etree.SubElement(root, "assignment")
+        hostIdElement = etree.SubElement(assignmentElement, "host_id")
+        wuIdElement = etree.SubElement(assignmentElement, "wu_id")
+        resultIdElement = etree.SubElement(assignmentElement, "result_id")
+        hostIdElement.text = str(assignment.hostId)
+        wuIdElement.text = str(assignment.wuId)
+        resultIdElement.text = str(assignment.resultId)    
+
+    outFile = open(filename, "w")
+    
+    if(len(assignments) > 0):        
+        outFile.write(etree.tostring(root, pretty_print=True))
+    else:
+        outFile.write("<xs_output>")
+        outFile.write("<xs_output/>")
+            
+    outFile.close()
+    print(etree.tostring(root, pretty_print=True))
     
 
-outFile = open("xs_output.xml", "w")
-outFile.write(etree.tostring(root, pretty_print=True))
-outFile.close()
-print(etree.tostring(root, pretty_print=True))
-"""
-   
-#TODO: grab list of app_versions, assignment, wu_results, nodes
-#sort lists
-    #nodes: highest demand to lowest for work + availability
-    #wu_results: highest fpops to lowest
+def main():
+    xsInput = readSchedulerInput("xs_input.xml")
+    wuResults = readWorkunitResults(xsInput)
+    requests = readRequests(xsInput)
+    assignments = scheduleWork(wuResults, requests)
+    writeSchedulerOutput("xs_output.xml", assignments)
+
+    
+main()
+#roundrobin()
+    
